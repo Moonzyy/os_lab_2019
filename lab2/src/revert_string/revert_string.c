b8c9f73522f967fcf2543669b91de5253475ce43
Moonzyy <>
2021-06-16 12:33:11 +0000

    add lab1 lab2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,15 @@
 
 void RevertString(char *str)
 {
-	// your code here
+  int size = 0;
+	while(str[size])
+    size ++;
+  char t;
+  for(int i = 0; i < size/2;i++)
+  {
+    t = str[i];
+    str[i] = str[size-i-1];
+    str[size-i-1] = t;
+  }
 }
 
